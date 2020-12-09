4c14532f22246e88b7b06fbc4cc0150d5828b65a
Yosuke Kimura <yosukefk@users.noreply.github.com>
2020-12-09 15:28:56 -0600

    backgroun manager read_projection() method

--- plotter/plotter_background.py
+++ plotter/plotter_background.py
@@ -19,75 +19,16 @@ class BackgroundManager:
         :param wms_options: arguments to GeoAxes.add_wms()
         """
         if bgfile is None:
-            if any((projection is None, extent is None)):
-                if wms_options:
-                    # wms to add background, use data's projection/extent
-                    self.wms_options = wms_options
-                else:
-                    # NULL background!!!
-                    # raise RuntimeError('Null background!!')
-                    self.wms_options = None
-                self.projection = projection
-                self.extent = extent
-                self.img = None
-            else:
-                self.projection = projection
-                self.extent = extent
-                self.img = None
-                self.wms_options = wms_options
+            self.projection = projection
+            self.extent = extent
+            self.img = None
+            self.wms_options = wms_options
         else:
             # use bgfile's extent
             self.b = rasterio.open(bgfile)
 
-            # try interpret projection of raster...  this should be supported by cartopy,
-            # something like this https://github.com/SciTools/cartopy/pull/1023, which kind of superceded by
-            # https://github.com/SciTools/cartopy/issues/1477 which is still taking time...  So for now i do
-            # quick-and-dirty job here
-            # this is good source too https://github.com/djhoese/cartopy/blob/feature-from-proj/lib/cartopy/_proj4.py
-            # but he manually eddite crs.py to interpret from proj4 components and i dont think i can import his work easily.
             if source_projection is None:
-                crs_data = self.b.crs.data
-                if 'init' in crs_data and crs_data['init'].lower().startswith('epsg:'):
-                    # if epsg is specified, assume it is going to work.
-                    epsg = int(crs_data['init'][5:])
-                    self.source_projection = ccrs.epsg(epsg)
-                elif 'proj' in crs_data:
-                    _GLOBE_PARAMS = {'datum': 'datum',
-                                     'ellps': 'ellipse',
-                                     'a': 'semimajor_axis',
-                                     'b': 'semiminor_axis',
-                                     'f': 'flattening',
-                                     'rf': 'inverse_flattening',
-                                     'towgs84': 'towgs84',
-                                     'nadgrids': 'nadgrids',
-                                     'R': 'semimajor_axis',
-                                     }
-                    projection_terms = {}
-                    globe_terms = {}
-                    for name, value in crs_data.items():
-                        if name in _GLOBE_PARAMS:
-
-                            globe_terms[name] = value
-                        else:
-                            projection_terms[name] = value
-                    # yk, without this, it defaults to wgs84...
-                    globe_terms.setdefault('ellps', None)
-                    globe = ccrs.Globe(**{_GLOBE_PARAMS[name]: value for name, value in
-                                          globe_terms.items()})
-                    if crs_data['proj'] == 'lcc':
-                        # there seems to be more subtleteis , but this should suffice
-                        # https://github.com/djhoese/cartopy/blob/feature-from-proj/lib/cartopy/crs.py#L1144-L1172
-                        self.source_projection = ccrs.LambertConformal(
-                            central_longitude=crs_data['lon_0'],
-                            central_latitude=crs_data['lat_0'],
-                            false_easting=crs_data['x_0'],
-                            false_northing=crs_data['y_0'],
-                            standard_parallels=[crs_data['lat_1'], crs_data['lat_2']],
-                            globe=globe)
-                    else:
-                        raise NotImplementedError(f"proj4 projection '{crs_data['proj']}'")
-                else:
-                    raise RuntimeError("cant tell projection of raster, use 'source_projection' to specify")
+                self.source_projection = self.read_projection(self.b.crs.data)
             else:
                 self.source_projection = source_projection
 
@@ -111,6 +52,66 @@ class BackgroundManager:
                 else:
                     # user specified both projection and extent
                     self.extent = extent
+
+    @staticmethod
+    def read_projection(crs_data):
+        # try interpret projection of raster...  this should be supported by cartopy,
+        # something like this https://github.com/SciTools/cartopy/pull/1023, which kind of superceded by
+        # https://github.com/SciTools/cartopy/issues/1477 which is still taking time...  So for now i do
+        # quick-and-dirty job here
+        # this is good source too https://github.com/djhoese/cartopy/blob/feature-from-proj/lib/cartopy/_proj4.py
+        # but he manually eddite crs.py to interpret from proj4 components and i dont think i can import his work easily.
+
+        if 'init' in crs_data and crs_data['init'].lower().startswith('epsg:'):
+            # if epsg is specified, assume it is going to work.
+            epsg = int(crs_data['init'][5:])
+            source_projection =  ccrs.epsg(epsg)
+
+        elif 'proj' in crs_data:
+
+            _GLOBE_PARAMS = {'datum': 'datum',
+                             'ellps': 'ellipse',
+                             'a': 'semimajor_axis',
+                             'b': 'semiminor_axis',
+                             'f': 'flattening',
+                             'rf': 'inverse_flattening',
+                             'towgs84': 'towgs84',
+                             'nadgrids': 'nadgrids',
+                             'R': 'semimajor_axis',
+                             }
+            projection_terms = {}
+            globe_terms = {}
+            for name, value in crs_data.items():
+                if name in _GLOBE_PARAMS:
+                    globe_terms[name] = value
+                else:
+                    projection_terms[name] = value
+
+            # yk, without this, it defaults to wgs84...
+            globe_terms.setdefault('ellps', None)
+            globe = ccrs.Globe(**{_GLOBE_PARAMS[name]: value for name, value in
+                                  globe_terms.items()})
+
+            if crs_data['proj'] == 'lcc':
+                # there seems to be more subtleteis , but this should suffice
+                # https://github.com/djhoese/cartopy/blob/feature-from-proj/lib/cartopy/crs.py#L1144-L1172
+                source_projection = ccrs.LambertConformal(
+                    central_longitude=crs_data['lon_0'],
+                    central_latitude=crs_data['lat_0'],
+                    false_easting=crs_data['x_0'],
+                    false_northing=crs_data['y_0'],
+                    standard_parallels=[crs_data['lat_1'], crs_data['lat_2']],
+                    globe=globe)
+            elif False:
+                # define more projection as needed
+                pass
+            else:
+                raise NotImplementedError(f"proj4 projection '{crs_data['proj']}'")
+        else:
+
+            raise RuntimeError("can't tell projection of raster, use 'source_projection' to specify")
+
+        return source_projection
 
     def warp(self, projection):
         transform, width, height = calculate_default_transform(
