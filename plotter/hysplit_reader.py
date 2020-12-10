4d23df2fcd6183117b2eb78281e6daa203d0d994
Yosuke Kimura <yosukefk@users.noreply.github.com>
2020-12-10 12:47:20 -0600

    hysplit_reader.py runs

--- plotter/hysplit_reader.py
+++ plotter/hysplit_reader.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import datetime
+import pytz
 from functools import reduce
 
 
 
-def Reader(fnames):
-    assert len(fnames) == 11
-    data = [pd.read_csv(_, sep='\s+') for _ in fnames]
-    data = reduce(lambda p,q: p.merge(q), data)
+def Reader(f):
+    data = pd.read_csv(f, sep='\s+')
 
     v = data.iloc[:, 9:]
+    ts = [datetime.datetime(*_).replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Etc/GMT+6')) for _ in data.iloc[:, 1:6].itertuples(index=None)]
 
-    return {'v': v}
+
+    return {'v': v, 'ts': ts, 'df': data}
 
 def tester():
-    x = Reader( "outconc.S2.pulse.NAM.txt")
+    with open( "../data/outconc.S2.pulse.NAM.txt") as f:
+        x = Reader(f)
+    return x
+if __name__ == '__main__':
+    x = tester()
