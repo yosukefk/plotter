3751c334533ceb2b468c7aa19f126f1071161773
Yosuke Kimura <yosukefk@users.noreply.github.com>
2020-12-10 13:55:49 -0600

    hysplit_reader.py

---
+++ demo/work_pretty_solo_hysplit.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+import sys
+
+sys.path.append('..')
+
+from plotter import hysplit_reader
+import plotter.plotter_solo as plotter_solo
+from plotter.plotter_util import LambertConformalTCEQ
+from plotter.plotter_background import BackgroundManager
+
+import geopandas as gpd
+import matplotlib as mpl
+import matplotlib.colors as colors
+from shapely.geometry import Polygon
+from adjustText import adjust_text
+import numpy as np
+
+from pathlib import Path
+from multiprocessing import Pool
+import shlex
+import subprocess
+import socket
+
+# save better resolution image 
+mpl.rcParams['savefig.dpi'] = 300
+
+# input directory/file names
+ddir = Path('../data')
+# input file name
+fname = 'outconc.S2.pulse.NAM.txt'
+
+# intermediate
+wdir = Path('./img')
+
+# output
+odir = Path('.')
+oname = 'outconc.S2.pulse.NAM.mp4'
+
+# prep workdir
+if not wdir.is_dir():
+    wdir.mkdir()
+else:
+    for f in wdir.glob('*.png'):
+        try:
+            f.unlink()
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+
+# aux inputs
+bgfile = '../resources/naip_toy_pmerc_5.tif'
+shpfile = '../resources/emitters.shp'
+
+
+# source locations
+df_shp = gpd.read_file(shpfile)
+df_shp = df_shp.to_crs('EPSG:3857')
+
+# title to use
+title = 'Unintented Pulsated S2'
+
+with open(ddir / fname) as f:
+    #dat = hysplit_reader.Reader(f)
+    x = np.arange(34)*0.1 -464.35
+    y = np.arange(47)*0.1 -996.65
+    dat = hysplit_reader.Reader(f, x=x, y=y)
+
+# grab necessary info
+arr = dat['v']
+tstamps = dat['ts']
+grid = dat['grid']
+
+# get horizontal extent 
+extent = [
+    grid['x0'], grid['x0'] + grid['nx'] * grid['dx'],
+    grid['y0'], grid['y0'] + grid['ny'] * grid['dy'],
+]
+
+# distance in calpost is in km
+extent = [_ * 1000 for _ in extent]
+x = dat['x'] * 1000
+y = dat['y'] * 1000
+
+if dat['units'] == 'g/m**3':
+    # convert unit of array from g/m3 tp ppb
+    # mwt g/mol
+    # molar volume m3/mol
+    arr = arr / 16.043 * 0.024465403697038 * 1e9
+elif dat['units'] == 'ppb':
+    pass
+else:
+    raise ValueError(f"unknown mass unit: '{dat['units']}'")
+
+# Mrinali/Gary's surfer color scale
+cmap = colors.ListedColormap([
+    '#D6FAFE', '#02FEFF', '#C4FFC4', '#01FE02',
+    '#FFEE02', '#FAB979', '#EF6601', '#FC0100', ])
+cmap.set_under('#FFFFFF')
+cmap.set_over('#000000')
+# Define a normalization from values -> colors
+bndry = [1, 10, 50, 100, 200, 500, 1000, 2000]
+norm = colors.BoundaryNorm(bndry, len(bndry))
+
+plotter_options = {
+    'background_manager': BackgroundManager(bgfile=bgfile,),
+    'title': title,
+    'contour_options': {
+        'levels': bndry,
+        'cmap': cmap,
+        'norm': norm,
+        'alpha': .5,
+        'extend': 'max',
+    },
+    'colorbar_options': {
+        'label': r'$CH_4$ (ppbV)',
+    },
+    'customize_once': [
+        # emission points
+        lambda p: df_shp.plot(ax=p.ax, column='kls', categorical=True, legend=False, zorder=10,
+                          markersize=2,
+                          # got red/blue/yellow from colorbrewer's Set1
+                          cmap=colors.ListedColormap(['#e41a1c', '#377eb8', '#ffff33'])
+                          ),
+
+        # emission point annotations
+        lambda p: 
+            # adjust_text() repels labels from each other
+            adjust_text(
+            # make list of annotation
+            list(
+                # this part creates annotation for each point
+                p.ax.annotate(_.Site_Label, (_.geometry.x, _.geometry.y,),
+                    zorder=11, 
+                    fontsize=4
+                    )
+                # goes across all points but filter by Site_Label
+                for _ in df_shp.itertuples()
+            ),
+        ),
+        # modeled box
+        lambda p: p.ax.add_geometries(
+            [Polygon([(extent[x], extent[y]) for x, y in ((0, 2), (0, 3), (1, 3), (1, 2), (0, 2))])],
+            crs=LambertConformalTCEQ(), facecolor='none', edgecolor='white', lw=.6,
+        ),
+
+    ]}
+
+# make a plot template
+p = plotter_solo.Plotter(array=arr, tstamps=tstamps, 
+                         x=x, y=y, projection=LambertConformalTCEQ(),
+                         plotter_options=plotter_options)
+
+
+# function to save one time frame
+def saveone(i, pname=None):
+    if pname is None: pname = wdir / f'{i:04}.png'
+
+    ts = tstamps[i]
+    footnote = str(ts)
+    p(pname, tidx=i, footnote=footnote)
+
+# make single image file (for QA)
+print(len(tstamps))
+saveone(min(16*60, len(tstamps)-1), (odir / oname).with_suffix('.png'))
+
+# you decide if you want to use many cores
+# parallel processing
+# save all frames in parallel
+# 68 for stampede, 24 for ls5
+nthreads = 24  # ls5
+
+# except that you are on login node
+hn = socket.getfqdn()
+if hn.startswith('login') and '.tacc.' in hn:
+    ntheads = 1
+
+if nthreads > 1:
+    with Pool(nthreads) as pool:
+        pool.map(saveone, range(len(tstamps)))
+else:
+    # serial processing
+    for i in range(len(tstamps)):
+        saveone(i)
+
+# make mpeg file
+cmd = f'ffmpeg -i "{wdir / "%04d.png"}" -vframes 2880 -crf 3 -vcodec libx264 -pix_fmt yuv420p -f mp4 -y "{odir / oname}"'
+subprocess.run(shlex.split(cmd), check=True)
--- plotter/hysplit_reader.py
+++ plotter/hysplit_reader.py
@@ -5,14 +5,30 @@ from functools import reduce
 
 
 
-def Reader(f):
+def Reader(f, tslice=slice(None,None), x=None, y=None):
     data = pd.read_csv(f, sep='\s+')
+    data = data.iloc[tslice, :]
+    units = 'ppb'
 
-    v = data.iloc[:, 9:]
-    ts = [datetime.datetime(*_).replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Etc/GMT+6')) for _ in data.iloc[:, 1:6].itertuples(index=None)]
+    v = data.iloc[:, 9:].to_numpy()
+    ts = [ datetime.datetime(_[0]+200, *_[1:]).replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Etc/GMT+6')) for _ in data.iloc[:, 1:6].itertuples(index=None)]
+
+    if not all((x is None, y is None)):
+        nx, ny = len(x), len(y)
+        grid = {'x0': x.min(), 'x1': x.max(), 'y0': y.min(), 'y1':y.max(),
+                'nx': nx, 'ny': ny, 'dx': x[1]-x[0],
+                'dy':y[1]-y[0]}
+        v = v.reshape(-1, ny, nx)
+
+        dct0 = {'x': x, 'y': y, 'grid':grid}
 
 
-    return {'v': v, 'ts': ts, 'df': data}
+    dct = {'v': v, 'ts': ts, 'units': units, 'df': data}
+    if dct0:
+        dct.update(dct0)
+
+    return dct
+
 
 def tester():
     with open( "../data/outconc.S2.pulse.NAM.txt") as f:
