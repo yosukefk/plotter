228f09072364559568babe0fedd668e0a46b1a8f
Yosuke Kimura <yosukefk@users.noreply.github.com>
2021-04-15 21:26:20 -0500

    savemp4 still doesnt work w Pool

--- plotter/plotter_util.py
+++ plotter/plotter_util.py
@@ -51,53 +51,61 @@ class background_adder:
     def refresh_background(self, p):
         p.background_bga.set_zorder(1)
 
-def savemp4(p, saveone=None, nthreads=None, odir='.', oname='animation.mp4'):
+def savemp4(p, wdir=None, nthreads=None, odir='.', oname='animation.mp4'):
     '''save mp4
     :param plotter:
     '''
 
     # TODO allow passing wdir, in case user wants to hold onto them
 
-    with tempfile.TemporaryDirectory() as wdir:
+    if wdir is None:
+        is_tempdir = True
+        wdir = tempfile.TemporaryDirectory()
+    else:
+        is_tempdir = False
+        wdir.mkdir(exist_ok=False)
 
-        # you decide if you want to use many cores
-        # parallel processing
-        # save all frames in parallel
-        # 68 for stampede, 24 for ls5
-        if nthreads is None:
-            nthreads = 24  # ls5
+    # you decide if you want to use many cores
+    # parallel processing
+    # save all frames in parallel
+    # 68 for stampede, 24 for ls5
+    if nthreads is None:
+        nthreads = 24  # ls5
 
-        if saveone is None:
-            #saveone = lambda i: p.save(Path(wdir) / f'{i:04}.png', tidx=i)
-            def saveone(i):
-                p.savefig(Path(wdir) / png_fmt_py.format(i), tidx=i)
+# FIXME this cannot be pickled, and doewnt work with Pool()
+# lambda is the same thing
+#    def saveone(i):
+#        p.savefig(Path(wdir) / png_fmt_py.format(i), tidx=i)
 
-        # except that you are on TACC login node
-        hn = socket.getfqdn()
-        if hn.startswith('login') and '.tacc.' in hn:
-            nthreads = 1
+    # except that you are on TACC login node
+    hn = socket.getfqdn()
+    if hn.startswith('login') and '.tacc.' in hn:
+        nthreads = 1
 
-        nframes = len(p.tstamps)
-        # '{:04d}.png' for python
-        # '%04d.png' for shell
-        png_fmt_py = '{:0' + str(int(np.log10(nframes) + 1)) + 'd}.png'
-        png_fmt_sh = '%0' + str(int(np.log10(nframes) + 1)) + 'd.png'
+    nframes = len(p.tstamps)
+    # '{:04d}.png' for python
+    # '%04d.png' for shell
+    png_fmt_py = '{:0' + str(int(np.log10(nframes) + 1)) + 'd}.png'
+    png_fmt_sh = '%0' + str(int(np.log10(nframes) + 1)) + 'd.png'
 
-        if nthreads > 1:
-            with Pool(nthreads) as pool:
-                pool.map(saveone, range(nframes))
-        else:
-            # serial processing
-            for i in range(nframes):
-                saveone(i)
-
-
-
-
-
-        # make mpeg file
-        #cmd = f'ffmpeg -i "{Path(wdir) / "%04d.png"}" -vframes {nframes} -crf 3 -vcodec libx264 -pix_fmt yuv420p -f mp4 -y "{Path(odir) / oname}"'
-        cmd = f'ffmpeg -i "{Path(wdir) / png_fmt_sh }" -vframes {nframes} -crf 3 -vcodec libx264 -pix_fmt yuv420p -f mp4 -y "{Path(odir) / oname}"'
-        subprocess.run(shlex.split(cmd), check=True)
+    if nthreads > 1:
+        with Pool(nthreads) as pool:
+            pool.map(saveone, range(nframes))
+            #pool.map(lambda i: p.savefig(Path(wdir) / pnt_fmt_py.format(i),
+            #    tidx=i), range(nframes))
+    else:
+        # serial processing
+        for i in range(nframes):
+            saveone(i)
 
 
+    # make mpeg file
+    #cmd = f'ffmpeg -i "{Path(wdir) / "%04d.png"}" -vframes {nframes} -crf 3 -vcodec libx264 -pix_fmt yuv420p -f mp4 -y "{Path(odir) / oname}"'
+    cmd = f'ffmpeg -i "{Path(wdir) / png_fmt_sh }" -vframes {nframes} -crf 3 -vcodec libx264 -pix_fmt yuv420p -f mp4 -y "{Path(odir) / oname}"'
+    subprocess.run(shlex.split(cmd), check=True)
+
+    if is_tempdir:
+        wdir.cleanup()
+
+#def _saveone(i):
+# do i have to make p globa variable...?
