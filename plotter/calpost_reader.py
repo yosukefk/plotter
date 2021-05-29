88a500a5f05ac1ec1415128aba014debc42e6cb2
Yosuke Kimura <yosukefk@users.noreply.github.com>
2021-05-29 16:18:42 -0500

    code/comment cleaned

--- plotter/calpost_reader.py
+++ plotter/calpost_reader.py
@@ -8,128 +8,18 @@ import re
 import pytz
 import warnings
 
+
 class cprValueError(ValueError): pass
 
 
 # old name...
-def Reader(f, tslice=slice(None,None), x=None, y=None):
+def Reader(f, tslice=slice(None, None), x=None, y=None):
     warnings.warn('use calpost_reader()', DeprecationWarning)
     return calpost_reader(f, tslice, x, y)
 
 
-def calpost_cat(lst):
-    # catenates list of calpost data, assuming they are continuous time
-    # series
-
-
-    # all the  test  making sure that datasets are compaible!
-
-    # dict_keys(['name', 'units', 'ts', 'grid', 'y', 'x', 'v', 'ptid'])
-
-    chk = {}
-    # name, units
-    chk['name'] = all((_['name'] == lst[0]['name']) for _ in lst)
-    chk['units'] = all((_['units'] == lst[0]['units']) for _ in lst)
-    
-    # test grid is identical
-    chk['grid'] = all((_['grid'] == lst[0]['grid']) for _ in lst)
-    chk['x'] = all((all(_['x'] == lst[0]['x'])) for _ in lst)
-    chk['y'] = all((all(_['y'] == lst[0]['y'])) for _ in lst)
-
-    # ptid not checked, because x, y are checked
-
-
-    if not all(chk.values()):
-        msg = ', '.join([_ for _ in chk if not chk[_]])
-        
-        raise cprValueError('incompatible header: {}'.format(msg))
-
-    # time step size
-    td = [_['ts'][1] - _['ts'][0] for _ in lst]
-    #print('td (timestep size) =',td)
-    #print('(td == td[0]) = ',[_ == td[0] for _ in td])
-    if not all([_ == td[0] for _ in td]):
-        raise cprValueError('incompatible time step size: {}'.format(td))
-
-
-    # done testing, find overlap of time period
-
-    def get_overlap(x, y, td):
-        if x[-1] + td == y[0]:
-            return 0
-
-        pos = (x==y[0]).argmax()
-        if pos == 0:
-            if x[0] == y[0]:
-                #raise cprValueError('covers identical time period')
-                return -1
-            else:
-                #raise cprValueError('time period has no overlap')
-                return -2
-        return len(x) - pos
-
-    overlaps = [get_overlap(x['ts'],y['ts'], td[0]) for x,y 
-            in zip(lst[:-1], lst[1:])]
-    msg1 = ''
-    msg2 = ''
-
-    #print('overlaps=',overlaps)
-    if any(_ < 0 for _ in overlaps ):
-        pos = ['{}/{}'.format(p,p+1) for x in range(len(lst)-1)]
-        msg1 = ', '.join([pos[_] for _ in overlaps if _ == -1])
-        msg2 = ', '.join([pos[_] for _ in overlaps if _ == -2])
-        if msg1: msg1 = 'non-advancing: ' + msg1
-        if msg2: msg2 = 'non-contiguous: ' + msg2
-        msg = '; '.join([msg1, msg2])
-        raise cprValueError('incompative time series: {}'.format(msg))
-
-    # time series look ok, ready to roll!
-
-    # get all the data from first one
-    dat = {k:v for k,v in lst[0].items()}
-
-
-    # v and ts need to be overwritten by catenated array
-
-    #chop = [None if _ == 0 else -_ for _ in overlaps]
-
-    #dat['ts'] = np.concatenate(
-    #        [d['ts'][:c] for d,c in 
-    #            zip(lst[:-1], chop)] + 
-    #        [ lst[-1]['ts'] ])
-
-    #dat['v'] = np.concatenate(
-    #        [d['v'][:c] for d,c in 
-    #            zip(lst[:-1], chop)] 
-    #            + [ lst[-1]['v'] ])
-
-    chop = [None if _ == 0 else -_ for _ in overlaps]
-
-    dat['ts'] = np.concatenate(
-        [lst[0]['ts']] +
-        [d['ts'][o:] for d,o in
-         zip(lst[1:],overlaps)]
-    )
-
-    dat['v'] = np.concatenate(
-        [lst[0]['v']] +
-        [d['v'][o:] for d,o in
-         zip(lst[1:],overlaps)]
-    )
-    return dat
-
-
-
-
-
-
-
-
-
-
-
 def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
-    '''reads calpost tseries output file (gridded recep), returns dict of numpy arrays
+    """reads calpost tseries output file (gridded recep), returns dict of numpy arrays
 
     :param FileIO f: opened calpost tseries file
     :param slice tslice: slice of time index
@@ -138,7 +28,7 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
 
     :return: dict, with ['v'] has data as 3d array (t, y, x)
     :rtype: dict
-    '''
+    """
 
     # assume file name passed if 'f' is string
     if isinstance(f, (str, Path)):
@@ -147,24 +37,22 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
 
     # read each input, and then cat
     if isinstance(f, list):
-        dat = [calpost_reader(_, slice(None,None), x, y) for _ in f]
+        dat = [calpost_reader(_, slice(None, None), x, y) for _ in f]
         dat = calpost_cat(dat)
-        print('ts.shp=',dat['ts'].shape)
-        print('v.shp=',dat['v'].shape)
-        print('ts.typ=',type(dat['ts']))
-        print('v.typ=',type(dat['v']))
-        print('s=',tslice)
-        print( dat['ts'][tslice].shape)
+        print('ts.shp=', dat['ts'].shape)
+        print('v.shp=', dat['v'].shape)
+        print('ts.typ=', type(dat['ts']))
+        print('v.typ=', type(dat['v']))
+        print('s=', tslice)
+        print(dat['ts'][tslice].shape)
         print(dat['v'][tslice].shape)
-        print( dat['ts'][60:].shape)
+        print(dat['ts'][60:].shape)
         print(dat['v'][60:].shape)
         dat['ts'] = dat['ts'][tslice]
         dat['v'] = dat['v'][tslice]
-        print('ts.shp=',dat['ts'].shape)
-        print('v.shp=',dat['v'].shape)
+        print('ts.shp=', dat['ts'].shape)
+        print('v.shp=', dat['v'].shape)
         return dat
-
-
 
     name = next(f)[31:]
     next(f)
@@ -192,9 +80,9 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
         'iy': iy,
         'x': x,
         'y': y,
-        'sxy': ['%d:%d' % (p,q) for (p,q) in zip(*[1000*np.round(_, 2) for _
-            in (x, y)])]
-        })
+        'sxy': ['%d:%d' % (p, q) for (p, q) in zip(*[1000 * np.round(_, 2) for _
+                                                     in (x, y)])]
+    })
     # hold on to each point's coordinates
     xr = x
     yr = y
@@ -214,14 +102,14 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
         # this is good, gridded data
         # print('GRID')
         pass
-    elif len(x)*len(y) == nx:
+    elif len(x) * len(y) == nx:
         # print('DESC')
         nx = len(x)
         ny = len(y)
-        
+
     else:
         # see if the data is subset of array
-        if len(x)*len(y) * .1 < nx:
+        if len(x) * len(y) * .1 < nx:
 
             nx = len(x)
             ny = len(y)
@@ -229,7 +117,7 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
             is_subregion = True
             idx = [(_ == x).argmax() for _ in xr]
             jdx = [(_ == y).argmax() for _ in yr]
-            map_subregion= [(j,i) for (j,i) in zip(jdx,idx)]
+            map_subregion = [(j, i) for (j, i) in zip(jdx, idx)]
 
         else:
             print('len(x),len(y)=', len(x), len(y))
@@ -260,7 +148,7 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
             # TODO
             vv = np.empty((ny, nx))
             vv[:] = np.nan
-            for ji,val in zip(map_subregion, v):
+            for ji, val in zip(map_subregion, v):
                 vv[ji] = val
         else:
             vv = v.reshape(ny, nx)
@@ -272,19 +160,102 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None):
             'x': x, 'v': v, 'ptid': ptid}
 
 
+def calpost_cat(lst):
+    """
+    concatenates list of calpost data, assuming they are continuous time series
+
+    :param lst: list of data from calpots_reader()
+    :return:  dict similar to the one from calpost_reader()
+    """
+
+    chk = {}
+    # name, units
+    chk['name'] = all((_['name'] == lst[0]['name']) for _ in lst)
+    chk['units'] = all((_['units'] == lst[0]['units']) for _ in lst)
+
+    # test grid is identical
+    chk['grid'] = all((_['grid'] == lst[0]['grid']) for _ in lst)
+    chk['x'] = all((all(_['x'] == lst[0]['x'])) for _ in lst)
+    chk['y'] = all((all(_['y'] == lst[0]['y'])) for _ in lst)
+
+    # ptid not checked, because x, y are checked
+
+    if not all(chk.values()):
+        msg = ', '.join([_ for _ in chk if not chk[_]])
+
+        raise cprValueError('incompatible header: {}'.format(msg))
+
+    # time step size
+    td = [_['ts'][1] - _['ts'][0] for _ in lst]
+    # print('td (timestep size) =',td)
+    # print('(td == td[0]) = ',[_ == td[0] for _ in td])
+    if not all([_ == td[0] for _ in td]):
+        raise cprValueError('incompatible time step size: {}'.format(td))
+
+    # done testing, find overlap of time period
+
+    def get_overlap(x, y, td):
+        if x[-1] + td == y[0]:
+            return 0
+
+        pos = (x == y[0]).argmax()
+        if pos == 0:
+            if x[0] == y[0]:
+                # raise cprValueError('covers identical time period')
+                return -1
+            else:
+                # raise cprValueError('time period has no overlap')
+                return -2
+        return len(x) - pos
+
+    overlaps = [get_overlap(x['ts'], y['ts'], td[0]) for x, y
+                in zip(lst[:-1], lst[1:])]
+
+    # print('overlaps=',overlaps)
+    if any(_ < 0 for _ in overlaps):
+        pos = ['{}/{}'.format(p, p + 1) for p in range(len(lst) - 1)]
+        msg1 = ', '.join([pos[_] for _ in overlaps if _ == -1])
+        msg2 = ', '.join([pos[_] for _ in overlaps if _ == -2])
+        if msg1: msg1 = 'non-advancing: ' + msg1
+        if msg2: msg2 = 'non-contiguous: ' + msg2
+        msg = '; '.join([msg1, msg2])
+        raise cprValueError('incompatible time series: {}'.format(msg))
+
+    # time series look ok, ready to roll!
+
+    # get all the data from first one
+    dat = {k: v for k, v in lst[0].items()}
+
+    # v and ts need to be overwritten by concatenated array
+
+    chop = [None if _ == 0 else -_ for _ in overlaps]
+
+    dat['ts'] = np.concatenate(
+        [lst[0]['ts']] +
+        [d['ts'][o:] for d, o in
+         zip(lst[1:], chop)]
+    )
+
+    dat['v'] = np.concatenate(
+        [lst[0]['v']] +
+        [d['v'][o:] for d, o in
+         zip(lst[1:], chop)]
+    )
+    return dat
+
+
 def tester():
     ddir = Path('../data')
     fname = 'tseries_ch4_1min_conc_co_fl.dat'
 
     with open(ddir / fname) as f:
-        dat = Reader(f, slice(12*60, 12*60+10))
+        dat = calpost_reader(f, slice(12 * 60, 12 * 60 + 10))
     return dat
 
-if __name__ == '__main__':
 
-    #dat = tester()
-    #print(dat)
+if __name__ == '__main__':
     import sys
+
     with open(sys.argv[1]) as f:
-        dat = Reader(f, slice(12*60, 12*60+10))
+        dat = calpost_reader(f, slice(12 * 60, 12 * 60 + 10))
     print(dat)
