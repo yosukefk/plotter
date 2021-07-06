25af026d6fc4cf5b3c3f0fd2ce940d2c1eee4237
yosukefk <yosuke@austin.utexas.edu>
2021-07-06 12:19:46 -0500

    PlotterEmpty

--- plotter/plotter_core.py
+++ plotter/plotter_core.py
@@ -17,6 +17,94 @@ import warnings
 
 mpl.use('Agg')
 
+class PlotterEmpty:
+    def __init__(self, array, tstamps, projection=None, extent=None, x=None, y=None,
+                 plotter_options=None):
+        self.fig = plotter_options.get('fig', plt.figure())
+        pos = plotter_options.get('pos', None)
+
+        self.arr = array
+        assert array is None
+        self.tstamps = tstamps
+
+        self.imshow_options = plotter_options.get('imshow_options', None)
+        self.contour_options = plotter_options.get('contour_options', None)
+
+        self.colorbar_options = plotter_options.get('colorbar_options', {})
+
+        self.footnote = plotter_options.get('footnote', None)
+        self.footnote_options = plotter_options.get('footnote_options', {})
+        self.footnote_manager = None
+
+        self.title = plotter_options.get('title', None)
+        self.title_options = plotter_options.get('title_options', None)
+
+        # create plots (GeoAxes)
+        if pos:
+            self.ax = self.fig.add_subplot(*pos)
+            self.ax.axis('off')
+        else:
+            # TODO fix this warning
+            # MatplotlibDeprecationWarning: Adding an axes using the same arguments as a previous axes currently reuses the earlier instance.
+            # In a future version, a new instance will always be created and returned.
+            # Meanwhile, this warning can be suppressed, and the future behavior ensured, by passing a unique label to each axes instance.
+            self.ax = self.fig.add_subplot()
+            self.ax.axis('off')
+
+        # self.title overrides 'label' in self.title_options
+        if self.title is None:
+            if self.title_options is None:
+                pass
+            else:
+                self.title = self.title_options.get('label')
+        else:
+            if self.title_options is None:
+                self.title_options = {'label': self.title}
+            else:
+                self.title_options['label'] = self.title
+
+        if self.title is not None:
+            ttlopt = {'loc': 'center'}
+            ttlopt.update(self.title_options)
+            self.ax.set_title(**ttlopt)
+
+        # other customizations
+        if 'customize_once' in plotter_options:
+            self.customize(plotter_options['customize_once'])
+
+        self.hasdata = False
+        self.im = None
+        self.cnt = None
+        self.mappable = None
+        self.current_arr = None
+        self.current_tstamp = None
+
+    def update(self, tidx=None, footnote=None, title=None):
+        """
+        Update plot to data at tidx
+
+        :param int tidx: time index
+        :param str footnote: footnote overwrite
+        :param str title:  title overwrite
+        """
+        if tidx is None: tidx = 0
+
+        if self.hasdata:
+            pass
+        else:
+            if self.footnote != '':
+                self.footnote_manager = pf.FootnoteManager(self, self.footnote,
+                                                        self.footnote_options)
+
+#            # other customizations
+#            if self.customize_once:
+#                self.customize(self.customize_once)
+
+            self.hasdata = True
+
+        if title is not None:
+            # update the title
+            self.ax.set_title(title)
 
 class PlotterCore:
     def __init__(self, array, tstamps, projection=None, extent=None, x=None, y=None,
