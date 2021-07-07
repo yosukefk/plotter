8ca70776385585b8e74563b45ee9f4f2e3337e5d
yosukefk <yosuke@austin.utexas.edu>
2021-07-07 10:34:32 -0500

    incremented versoin to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='plotter',
-        version='1.0.1',
+        version='1.1.0',
         description='make tile plot from camx/calpuff etc',
         url='https://github.com/yosukefk/plotter',
         license='MIT',
