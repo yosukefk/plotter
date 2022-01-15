65f489d2413c3a1d51893a04d3c09c1fdf044e56
yosukefk <yosukesabai@hotmail.com>
2022-01-15 15:00:14 -0600

    trying harder to follow CF convention

--- plotter/calpost_ncf.py
+++ plotter/calpost_ncf.py
@@ -1,3 +1,5 @@
+# this cf was tedious to work with, so i wrote capost_ncf2, which uses plan netCDF4 package.
+# it is still not following CF convention, seems like, it doesnt read by IDV correctly
 import cf
 import numpy as np
 
@@ -34,6 +36,7 @@ def create(dat):
     if len(dat['v'].shape) == 4:
         nt, nz, ny, nx = dat['v'].shape
         has_z = True
+        print(nt, nz, ny, nx)
     else:
         nt, ny, nx = dat['v'].shape
         has_z = False
@@ -41,6 +44,7 @@ def create(dat):
     print('a')
 
     domain_axisT = cf.DomainAxis(nt)
+    domain_axisT.nc_set_unlimited(True)
     domain_axisY = cf.DomainAxis(ny)
     domain_axisX = cf.DomainAxis(nx)
 
@@ -57,6 +61,9 @@ def create(dat):
         domain_axisZ = cf.DomainAxis(nz)
         domain_axisZ.nc_set_dimension('z')
         axisZ = v.set_construct(domain_axisZ)
+    print(type(domain_axisY))
+    print(dir(domain_axisY))
+    print(domain_axisY.identity())
 
     print('b')
 
@@ -72,7 +79,8 @@ def create(dat):
                                       'standard_name': 'projection_y_coordinate',
                                       'units': 'meters'
                                   })
-    dimT = cf.DimensionCoordinate(data=dat['ts'])
+    dimT = cf.DimensionCoordinate(data=dat['ts'],
+)
 
     dimT.nc_set_variable('time')
     dimY.nc_set_variable('y')
@@ -82,19 +90,44 @@ def create(dat):
         z = dat['z']
         dimZ = cf.DimensionCoordinate(data=z,
                                   properties={
-                                      'standard_name': 'projection_z_coordinate',
+                                      'standard_name': 'height',
                                       'units': 'meters'
                                   })
         dimZ.nc_set_variable('z')
     print('c')
 
-    v.set_construct(dimT)
-    v.set_construct(dimY)
-    v.set_construct(dimX)
+    dim_t = v.set_construct(dimT, axes=domain_axisT.identity())
+    dim_y = v.set_construct(dimY, axes=domain_axisY.identity())
+    dim_x = v.set_construct(dimX, axes=domain_axisX.identity())
     if has_z:
-        v.set_construct(dimZ)
+        v.set_construct(dimZ, axes=domain_axisZ.identity())
 
     print('d')
+
+    if has_z:
+        v.set_data_axes([axisT, axisZ, axisY, axisX])
+    else:
+        v.set_data_axes([axisT, axisY, axisX])
+
+
+    datum = cf.Datum(parameters={'earth_radius': 637000.0})
+
+    coordinate_conversion_h = cf.CoordinateConversion(
+        parameters={'grid_mapping_name': 'lambert_conformal_conic',
+        'standard_parallel': (38.5, 38.5),
+        'longitude_of_central_meridian': -97.5,
+        'latitude_of_projection_origin': 38.5,
+        })
+
+
+    horizontal_crs = cf.CoordinateReference(
+        datum=datum,
+        coordinate_conversion=coordinate_conversion_h,
+        coordinates=[dim_x, dim_y])
+
+    v.set_construct(horizontal_crs)
+
+    
 
     return v
 
