98d7ce677e51f30d1513009144374d0be631a568
yosukefk <yosukesabai@hotmail.com>
2022-04-21 15:08:32 -0500

    make color map to span all time's data by default, not t=0 only

--- plotter/plotter_core.py
+++ plotter/plotter_core.py
@@ -340,6 +340,10 @@ class PlotterCore:
             if self.colorbar_options is not None:
                 kwds = self.colorbar_options
                 if not self.mappable is None:
+                    if 'norm' not in kwds:
+                        # if user didnt explicitly specify norm, i use entire range of data to bound color
+                        self.mappable.set_clim(vmin=self.arr.min(), vmax=self.arr.max())
+
                     self.cb = plt.colorbar(mappable=self.mappable, ax=self.ax,
                                            **kwds)
                 else:
