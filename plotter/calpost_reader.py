29d5c017770ddfaa4a4b31ceccab1d35805a2943
yosukefk <yosukesabai@hotmail.com>
2022-11-19 22:56:27 -0600

    bugfix for confidential file

--- plotter/calpost_reader.py
+++ plotter/calpost_reader.py
@@ -111,7 +111,7 @@ def calpost_reader(f, tslice=slice(None, None), x=None, y=None, z=None,
         def __next__(self):
             line = next(self.file_obj)
             if self.confidential:
-                line = line[13:]
+                line = line[12:]
             return line
 
         def __iter__(self):
